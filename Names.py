d6c240bcf491996647564fbf2480a5cfd1abc4a1
David Hernandez <dhernandez@verizowireless.com>
2017-01-23 11:11:44 -0800

    Names.py

---
+++ Names.py
@@ -0,0 +1,8 @@
+#
+name1 = "New Jersey"
+name2 = "New York"
+name3 = "Pennsylvania"
+name4 = raw_input("Enter Fourth name: ")
+
+#print "{:>30 name1} {:>30 name2} {:>30 name3}"
+print "{:>30} {:>30} {:>30} {:>30}".format(name1, name2, name3, name4)
